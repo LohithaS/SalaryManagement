79558a17e80081467d1f8abc798b23654a168c73
Lohitha Singapilli <lsingapilli@corp.miraclesoft.com>
2019-05-14 14:54:25 +0530

    SalaryManagement code

---
+++ Salary_XOM/src/SalaryManagement.scala
@@ -0,0 +1,10 @@
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import ilog.rules.bom.annotations.NotBusiness
+
+@XmlAccessorType(XmlAccessType.FIELD)
+class SalaryManagement( name: String,basic: Double) {
+  var name: String = null;
+  var basic: Double = 0.0;
+
+}
